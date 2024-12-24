cda3fcfae523f537659534d20a87572563e7ab8f
Kauan Cardoso <kauanmartins977@gmail.com>
2024-12-24 13:35:26 -0300

    Criação da camada de serviços

---
+++ src/services/ITodoService.cs
@@ -0,0 +1,13 @@
+﻿using todo.src.model;
+
+namespace todo.src.services;
+
+public interface ITodoService
+{
+    IQueryable<Todo> GetAll();
+    IQueryable? GetTitle(string title);
+    Todo? GetId(int id);
+    void AddTodo(Todo todo);
+    void UpdateTodo(Todo todo);
+    void DeleteTodo(int id);
+}
---
+++ src/services/TodoService.cs
@@ -0,0 +1,27 @@
+﻿using todo.data.repository;
+using todo.src.model;
+using todo.src.services;
+
+namespace todo.src.services;
+
+public class TodoService(TodoRepository repository) : ITodoService
+{
+    private readonly ITodoRepository _repository = repository;
+
+    public void AddTodo(Todo todo) => _repository.AddTodo(todo);
+
+    public void DeleteTodo(int id)
+    {
+        var todo = _repository.GetId(id);
+
+        if (todo != null) _repository.DeleteTodo(todo);
+    }
+
+    public IQueryable<Todo> GetAll() => _repository.GetAll();
+
+    public Todo? GetId(int id) => _repository.GetId(id);
+
+    public IQueryable? GetTitle(string title) => _repository.GetTitle(title);
+
+    public void UpdateTodo(Todo todo) => _repository.UpdateTodo(todo);
+}
